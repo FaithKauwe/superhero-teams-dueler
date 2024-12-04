43b217939205c34e237dea87d8b0c5d30d0b7641
FaithKauwe <fkauwe@gmail.com>
2024-12-04 10:25:16 -0800

    P2 complete, add ability and armor classes

---
+++ ability.py
@@ -0,0 +1,16 @@
+import random
+
+class Ability:
+    def __init__(self, name: str, max_damage: int):
+        self.name = name
+        self.max_damage = max_damage
+    
+    def attack(self):
+        random_value = random.randint(0,self.max_damage)
+        return random_value
+    
+
+if __name__ == "__main__":
+    test_ability = Ability("fireball", 39)
+    print(test_ability.name)
+    print(test_ability.attack())
---
+++ armor.py
@@ -0,0 +1,17 @@
+import random
+
+class Armor:
+    def __init__(self, name: str, max_block: int):
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        random_value = random.randint(0,self.max_block)
+        return random_value
+    
+if __name__ == "__main__":
+  # If you run this file from the terminal
+  # this block is executed.
+  armor = Armor("Ice Shield", 10)
+  print(armor.name)
+  print(armor.block())
